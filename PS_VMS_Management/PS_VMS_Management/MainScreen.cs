672873e359895202c1bdad33cad9521dbbeba854
John Mac <imakaronas@gmail.com>
2018-02-08 14:58:59 +0200

    new commits

---
+++ PS_VMS_Management/PS_VMS_Management/MainScreen.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS_VMS_Management
+{
+    public partial class MainScreen : Form
+    {
+
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+            string sTitle =  String.Empty;
+            
+            Program myProgram = new Program();
+            myProgram.Find_IP_Address();
+
+            sTitle = string.Format("PS VM Management [I.P. : {0}]", myProgram.Find_IP_Address());
+            this.Text = sTitle;
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            SettingsScreen SetScreen = new SettingsScreen();
+            SetScreen.Show();
+        }
+
+        private void btnIISStart_Click(object sender, EventArgs e)
+        {
+            Program myProgram = new Program();
+            myProgram.DoIISStart();
+        }
+
+        private void btnIIStop_Click(object sender, EventArgs e)
+        {
+            Program myProgram = new Program();
+            myProgram.DoIISStop();
+        }
+
+        private void btnIISReset_Click(object sender, EventArgs e)
+        {
+            Program myProgram = new Program();
+            myProgram.DoIISReset();
+        }
+
+
+        private void btnNBG_Click(object sender, EventArgs e)
+        {
+            Program myProgramNBG = new Program();
+            myProgramNBG.Start_NBG_APS_Mock();
+        }
+
+        private void btnPBG_Click(object sender, EventArgs e)
+        {
+            Program myProgram = new Program();
+            myProgram.Start_PBG_APS_Mock();
+        }
+
+        private void btnPBG_OLIS_Click(object sender, EventArgs e)
+        {
+            Program myProgramOLIS = new Program();
+            myProgramOLIS.Start_PBG_OLIS_Mock();
+        }
+
+        private void btnEFG_Click(object sender, EventArgs e)
+        {
+            Program myProgramEFG = new Program();
+            myProgramEFG.Start_EFG_APS_Mock();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ PS_VMS_Management/PS_VMS_Management/SettingsScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS_VMS_Management
+{
+    public partial class SettingsScreen : Form
+    {
+        public SettingsScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsScreen_Load(object sender, EventArgs e)
+        {
+            string sTitle = String.Empty;
+            Program myProgram = new Program();
+            myProgram.Find_IP_Address();
+            sTitle = string.Format("PS VM Management [I.P. : {0}] - Settings Screen", myProgram.Find_IP_Address());
+            this.Text = sTitle;
+
+            Program MyProgram1 = new Program();
+
+            string sApplPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string sFullPath = sApplPath + @"\PS_VMS_Management.config.xml";
+
+            if (!File.Exists(sFullPath))
+            {
+                MyProgram1.CreateXMLfile();
+            }
+            
+            // MyProgram1.ReadXMLfile(@"C:\Development\GIT\PRNTC\PS_VMS_Management\PS_VMS_Management\bin\Debug\PS_VMS_Management.config.xml");
+            //MyProgram1.ReadXMLfile(@"C:\temp\PS_VMS_Management.config.xml");
+            MyProgram1.ReadXMLfile(sFullPath);
+            
+            textBox1.Text = MyProgram1.sNBGPath.ToString();
+            textBox2.Text = MyProgram1.sPBGPath.ToString();
+            textBox3.Text = MyProgram1.sEFGPath.ToString();
+            textBox4.Text = MyProgram1.sOLISPath.ToString();
+        }
+
+        private void btnSaveClose_Click(object sender, EventArgs e)
+        {
+
+            string sApplPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string sFullPath = sApplPath + @"\PS_VMS_Management.config.xml";
+
+            Program MyProgram = new Program();
+            MyProgram.UpdateXMLfile(sFullPath.ToString(), textBox1.Text.ToString(), textBox3.Text.ToString(), textBox2.Text.ToString(), textBox4.Text.ToString());
+
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
